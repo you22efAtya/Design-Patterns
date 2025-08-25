3e3a705e0481cd18831afbc431c75cb3094a40dc
you22efAtya <youssefmohamedyahoo.com9@gmail.com>
2025-08-26 00:16:14 +0300

    details

--- NotifcationSystemAfterFactoryDp/Program.cs
+++ NotifcationSystemAfterFactoryDp/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-
+            // the main program does not need to know about the concrete classes
             Console.WriteLine("Enter notification type (sms/email): ");
             var notificationType = Console.ReadLine();
             Console.WriteLine("Enter message to send: ");
